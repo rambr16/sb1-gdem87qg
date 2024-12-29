cbff0374f337dc5a6ee1a9880d066aa85730efa3
rambr16 <git@stackblitz.com>
2024-12-29 18:53:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-gdem87qg
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/rambr16/sb1-gdem87qg)
---
+++ src/store/auth.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+import { AuthState } from '../types';
+
+const VALID_CREDENTIALS = {
+  email: 'management@eagleinfoservice.com',
+  password: 'eisShared2025!'
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  isAuthenticated: false,
+  login: async (email: string, password: string) => {
+    if (email === VALID_CREDENTIALS.email && password === VALID_CREDENTIALS.password) {
+      set({ isAuthenticated: true });
+    } else {
+      throw new Error('Invalid credentials');
+    }
+  },
+  logout: () => set({ isAuthenticated: false })
+}));
